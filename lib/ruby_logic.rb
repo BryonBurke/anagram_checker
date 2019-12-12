774a0250c99237fd9a73afd324043d5a97c6bd90
Student <example@example.com>
2019-12-12 09:50:21 -0800

    add nickels and dimes. test successfully

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -2,34 +2,40 @@ class Change
   def initialize(amount_in_pennies)
     @amount_in_pennies = amount_in_pennies
     @quarters
-
+    @dimes
+    @nickels
   end
 
   def number_of_quarters()
     until (@amount_in_pennies < 25 )
-      @quarters = (@amount_in_pennies / 25).floor()
+      @quarters = (@amount_in_pennies / 25)
+      @amount_in_pennies = @amount_in_pennies % 25
+      puts @amount_in_pennies
       puts @quarters
       break
     end
     @quarters
-
-
-
   end
 
   def number_of_dimes()
-
-
+    until (@amount_in_pennies < 10 )
+      @dimes = (@amount_in_pennies / 10)
+      @amount_in_pennies = @amount_in_pennies % 10
+      puts  @amount_in_pennies
+      puts @dimes
+      break
+    end
+    @dimes
   end
 
   def number_of_nickels()
-
-
+    until (@amount_in_pennies < 5 )
+      @nickels = (@amount_in_pennies / 5)
+      @amount_in_pennies = @amount_in_pennies % 5
+      puts  @amount_in_pennies
+      puts  @nickels
+      break
+    end
+    @nickels
   end
-
-  def number_of_nickels()
-
-
-  end
-
 end
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -6,4 +6,12 @@ describe('#ruby_logic') do
     my_change = Change.new(117)
     expect(my_change.number_of_quarters).to(eq(4))
   end
+  it("finds the number of dimes") do
+    my_change = Change.new(117)
+    expect(my_change.number_of_dimes).to(eq(11))
+  end
+  it("finds the number of dimes") do
+    my_change = Change.new(117)
+    expect(my_change.number_of_nickels).to(eq(23))
+  end
 end
