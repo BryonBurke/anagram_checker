b85d0275eb9e3e163eea540b9e6825599a0c6ebd
Student <example@example.com>
2019-12-12 16:41:46 -0800

    add README

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-yup
+# _Ruby Week 1_
+#### _Make Change_
+****************
+## _December 12, 2019_
+****************
+#### _By: Joann Carter, Everett Lee-Wuollet Bryon Burke_
+***************
+# _Description_
+##### This is an application that will take a user's inputed dollar amount in pennies and return how many quarters, dimes, nickels, and pennies will be used for largest change possible.
+#  _Specification Chart_
+| Behavior  | Input  | Output  |
+|--------------|-----------|-----------|
+|finds the number of quarters|"117 "| "4"|
+|finds the number of dimes| "117" | "11" |
+|finds the number of nickels |"117"| "23"|
+|finds the number of pennies|"117"|"117"|
+|finds the final change|"117"| "quarters:4, dimes:1, nickels:2, pennies:2"|
+******************
+#### _Instructions_
+In the console:
+ * run "bundle"
+ * run "ruby ruby_scripts.rb"
+ * enter your dollar amount in pennies
+***************
+# _Technology Used_
+* Ruby
+* rspec
+*******
+###### _Licensing_
+MIT public
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -2,23 +2,23 @@ require('rspec')
 require('ruby_logic')
 
 describe('#ruby_logic') do
-  # it("finds the number of quarters") do
-  #   my_change = Change.new(117)
-  #   expect(my_change.number_of_quarters).to(eq(4))
-  # end
-  # it("finds the number of dimes") do
-  #   my_change = Change.new(117)
-  #   expect(my_change.number_of_dimes).to(eq(11))
-  # end
-  # it("finds the number of nickels") do
-  #   my_change = Change.new(117)
-  #   expect(my_change.number_of_nickels).to(eq(23))
-  # end
-  #
-  # it("finds the number of pennies") do
-  #   my_change = Change.new(117)
-  #   expect(my_change.number_of_pennies).to(eq(117))
-  # end
+  it("finds the number of quarters") do
+    my_change = Change.new(117)
+    expect(my_change.number_of_quarters).to(eq(4))
+  end
+  it("finds the number of dimes") do
+    my_change = Change.new(117)
+    expect(my_change.number_of_dimes).to(eq(11))
+  end
+  it("finds the number of nickels") do
+    my_change = Change.new(117)
+    expect(my_change.number_of_nickels).to(eq(23))
+  end
+
+  it("finds the number of pennies") do
+    my_change = Change.new(117)
+    expect(my_change.number_of_pennies).to(eq(117))
+  end
 
   it("finds the final change") do
     my_change = Change.new(117)
